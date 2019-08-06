7540f7ebc879b6cba6017f18edd88605ffb9e877
yanxin152133 <yanxin152133@gmail.com>
2019-08-06 21:51:50 +0800

    PHP语句分隔符

---
+++ php/chap01/sec01/end.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Create by PhpStorm
+ * Date: 2019/8/6
+ * Time: 21:38
+ * Features: 语句分隔符
+ */
+
+    // 语句结束符
+    // 动手试一下前一个语句不加语句结束符的概况和后一个语句不加语句结束符的概况
+    // echo 'hello world'
+    echo 'hello world';
