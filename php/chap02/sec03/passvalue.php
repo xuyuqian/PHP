c5a595dd46371ec5aa73daa2b9b62dd4569046fb
yanxin152133 <yanxin152133@gmail.com>
2019-08-07 16:08:27 +0800

    变量传值

---
+++ php/chap02/sec03/passvalue.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*
+ * Create by PhpStorm
+ * Author: Yan
+ * Date: 2019/8/7
+ * Time: 16:04
+ * Features: 变量传值
+ */
+
+	// 变量传值
+ 	// 值传递
+	$a=10;
+	$b=$a;
+	$b=5;
+	echo $a,$b,'</br>';
+
+	//引用传递
+	$c=10;
+	$d=&$c;
+	$c=5;
+	echo $c,$d,'</br>';
+
+ 
