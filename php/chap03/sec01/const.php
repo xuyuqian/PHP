aae9b49a914974303da2d52bea24ba8711a6ebb1
yanxin152133 <yanxin152133@gmail.com>
2019-08-07 16:44:20 +0800

    常量-特殊常量

---
+++ php/chap03/sec01/const.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/*
+ * Create by PhpStorm
+ * Author: Yan
+ * Date: 2019/8/7
+ * Time: 16:27
+ * Features: 常量
+ */
+
+	// 常量
+	// 使用函数定义常量:define
+	define('PI',3.14);
+
+	// 使用const关键字定义
+	const PII=3;
+
+	// 定义特使常量
+	define('-_-','small');
+	// const -_-='smali';   //错误
+
+ 
+ 
