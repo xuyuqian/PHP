e810f6ae684a815087e86558152b371fe1b967b1
yanxin152133 <yanxin152133@gmail.com>
2019-08-11 15:43:46 +0800

    while循环

---
+++ php/chap06/sec04/while.php
@@ -0,0 +1,26 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/11
+	 * Time: 15:30
+	 * Features: while循环
+	 */
+
+	// while循环
+
+	//定义条件
+	$i=1;
+
+	// 循环判定执行
+	while ($i<=10){
+		//循环体
+		echo $i++,'<br/>';
+
+		// 循环条件变更
+		// $i++;
+	}
+ 
+ 
