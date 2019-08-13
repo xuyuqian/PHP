70a566b16345c716c889ef73a8aa2c29456bbd20
yanxin152133 <yanxin152133@gmail.com>
2019-08-13 22:32:56 +0800

    文件加载路径

---
+++ php/chap09/sec01/path.php
@@ -0,0 +1,23 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/13
+	 * Time: 22:04
+	 * Features: 文件加载路径
+	 */
+
+	//PHP文件加载路径
+
+	//相对路径加载
+	include '../../chap08/sec01/include1.php';
+
+	echo $a;
+
+	// 绝对路径
+	include 'D:/github/PHP/php/chap08/sec01/include1.php';
+	echo PI;
+ 
+ 
