0a0899bbf92f99400baa3840d526ad59421912df
yanxin152133 <yanxin152133@gmail.com>
2019-08-14 17:22:54 +0800

    函数

---
+++ php/chap11/sec01/function1.php
@@ -0,0 +1,22 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/14
+	 * Time: 15:31
+	 * Features: 函数
+	 */
+
+	// 函数
+
+	// 定义函数
+	function display()
+	{
+		// 函数体
+		echo 'hello world';   //没有返回值
+	}
+
+	//调用函数
+	display();
