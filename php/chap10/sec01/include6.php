a0074c76560cb250869670220c13d1b9ac581ce7
yanxin152133 <yanxin152133@gmail.com>
2019-08-14 15:26:13 +0800

    文件嵌套包含

---
+++ php/chap10/sec01/include6.php
@@ -0,0 +1,15 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/14
+	 * Time: 14:58
+	 * Features: 文件嵌套包含
+	 */
+
+	// 文件嵌套包含
+
+	// 包含include3.php   文件本身包含了include4.php
+	include '../../chap08/sec02/include3.php';
