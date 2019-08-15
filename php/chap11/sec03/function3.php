7431ebff4b7bd8da74eef36a667b8905406c4262
yanxin152133 <yanxin152133@gmail.com>
2019-08-15 15:51:08 +0800

    函数

---
+++ php/chap11/sec03/function3.php
@@ -0,0 +1,41 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/15
+	 * Time: 14:36
+	 * Features: 函数返回值
+	 */
+
+	// 函数返回值
+
+	// 定义函数
+
+	function display()
+	{
+		//输出
+		echo __FUNCTION__;   //输出当前函数名字
+	}
+
+	var_dump(display());
+
+	// 加法运算
+	function add($num1, $num2)
+	{
+		return $num1 + $num2;    //返回结果
+
+		//输出
+		echo $num1;
+	}
+
+	$res = add(10, 20);     //外部定义变量接收函数运行结果
+	echo $res;
+	// 将结果提交给另外包含的文件
+	return 'hello world';
+
+
+	echo $res;
+
+
---
+++ php/chap11/sec03/function_include.php
@@ -0,0 +1,16 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/15
+	 * Time: 15:43
+	 * Features: 
+	 */
+
+	// 包含有return关键字的文件
+	$res = include_once 'function3.php';
+	var_dump($res);
+ 
+ 
