70b41ed830db567639b35a21caaf2db78272275b
yanxin152133 <yanxin152133@gmail.com>
2019-08-22 16:12:41 +0800

    冒泡排序

---
+++ php/chap16/sec03/BubbleSort.php
@@ -0,0 +1,31 @@
+<?php
+
+
+	/*
+	 * Create by PhpStorm
+	 * Author: Yan
+	 * Date: 2019/8/22
+	 * Time: 下午 4:00
+	 * Features: 冒泡排序
+	 */
+
+	// 冒泡排序
+
+	$arr = array(1, 4, 2, 9, 7, 5, 8);
+	//2. 想办法让下面可以每次找出最大值的代码重复执行
+	for ($i = 0, $len = count($arr); $i < $len; $i++) {
+		// 1. 想办法将最大的值放到最右边去
+		for ($j = 0, $len = count($arr); $j < $len - 1 - $i; $j++) {
+			//判断：两两相比
+			if ($arr[$j] > $arr[$j + 1]) {
+				//左边比右边大：交换
+				$temp = $arr[$j];
+				$arr[$j] = $arr[$j + 1];
+				$arr[$j + 1] = $temp;
+			}
+		}
+	}
+
+
+	echo '<pre>';
+	print_r($arr);
