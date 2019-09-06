433dfc67d2032667230d6626490cd19f43ddaf58
yanxin152133 <yanxin152133@gmail.com>
2019-09-06 12:18:39 +0800

    联合查询

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -510,3 +510,20 @@ select * from my_int where int_6 is null;
 # 获取所有姓小的同学
 select * from my_student where stu_name like '小%';
 select * from my_student where stu_name like '小_';
+
+# 获取男生身高升序，女生身高降序
+(select * from my_student where gender='男' order by stu_height asc limit 10)
+union
+(select * from my_student where gender='女' order by stu_height desc limit 10) ;
+
+select * from my_student
+union
+select * from my_student;
+
+select * from my_student
+union all
+select * from my_student;
+
+select stu_id,stu_name,stu_height from my_student
+union all
+select stu_height,stu_name,stu_id from my_student;
